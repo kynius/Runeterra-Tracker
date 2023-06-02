a1375910a7eb2e768c77143c8662f3077fd4bc5b
Krzysztof Gadzina <gadzinakrzys@gmail.com>
2023-06-02 15:51:23 +0200

    fixing axios error

--- src/Components/Match.js
+++ src/Components/Match.js
@@ -12,8 +12,7 @@ export default class Match extends Component {
             spells: [],
             queues: [],
             queue: {},
-            runes: [],
-            isLoading: true
+            runes: []
         }
     }
     checkNull(int, width)
@@ -73,9 +72,9 @@ export default class Match extends Component {
     async componentDidMount() {
         const response = await axios.get(this.state.url);
         const json = await response.data;
-        const responseRunes = await axios.get(`http://ddragon.leagueoflegends.com/cdn/${ddragonVersion}/data/en_US/runesReforged.json`);
+        const responseRunes = await axios.get(`https://ddragon.leagueoflegends.com/cdn/${ddragonVersion}/data/en_US/runesReforged.json`);
         const jsonRunes = await responseRunes.data;
-        const summonerSpellsResponse = await axios.get(`http://ddragon.leagueoflegends.com/cdn/${ddragonVersion}/data/en_US/summoner.json`).then(this.setState({isLoading: false}));
+        const summonerSpellsResponse = await axios.get(`https://ddragon.leagueoflegends.com/cdn/${ddragonVersion}/data/en_US/summoner.json`);
         const jsonSpells = await summonerSpellsResponse.data;
         this.setState({ queues: json, runes: jsonRunes, spells: jsonSpells.data});
         M.AutoInit();
@@ -135,14 +134,6 @@ export default class Match extends Component {
             opponentColor = 'win'
             opponentGameResult = 'Zwycięstwo'
         }
-            if(this.state.isLoading === true)
-            {
-                return (
-                    <>
-                        
-                    </>
-                )
-            }
         return (
             <>
                 <li>
